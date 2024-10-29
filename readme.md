c714ef262d6012bea89d4cefc8066459c29c1025
notsnip <snipc.mail@gmail.com>
2024-10-29 22:43:45 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,88 @@
+# Snip's Workstation Configuration  for Ubuntu
+
+## Overview
+
+This script automates the setup of a fresh Ubuntu installation, providing a convenient way to install essential software and customize system settings. It streamlines the configuration process which helps me to quickly get my development environment up and running.
+
+## What This Script Does
+
+- Installs essential system programs and updates the system.
+- Configures the desktop environment with themes, icons, and wallpapers.
+- Installs a variety of applications tailored for development, media consumption, and productivity.
+- Configures Git with user-defined settings.
+- Sets up development tools and runtimes.
+
+## Automatic Setup
+
+The script performs the following actions automatically:
+
+1. **System Updates**: Updates the package list and upgrades installed packages.
+2. **GNOME Tweaks and Extensions**: Installs GNOME Tweaks and the GNOME Shell Extension Manager for desktop customization.
+3. **Theming**: Sets up GTK themes, icons, and wallpapers.
+4. **Application Installation**: Installs various applications through Flatpak and Snap.
+5. **Development Tools**: Installs build tools, runtimes, and configures Git.
+6. **Prompt for Additional Installations**: Asks the user whether to install specific applications and tools.
+
+## Applications Installed
+
+The script installs the following applications automatically:
+
+- **Browsers & Media Players**:
+  - Zen Browser
+  - Warehouse (Flatpak tool)
+  - Amberol (Audio Player)
+  - Fotema (Photo Gallery)
+  - Clapper (Video Player)
+  
+- **Development Tools**:
+  - Visual Studio Code (Snap)
+  - ArmCord - Discord Client
+  - OBS Studio (optional)
+  - Blender (optional)
+  - MongoDB Compass (optional)
+  - Warp Terminal (optional)
+
+## System Requirements
+
+This script is intended for **Ubuntu** and has been tested on **Ubuntu 22.10**. 
+
+### Execution Directory
+
+Before running the script, make sure to navigate to the following directory:
+
+```bash
+cd ~/Downloads/workstation-config
+```
+
+## Manual Configuration After Installation
+
+After the installation completes, you will need to manually:
+
+- Install GNOME extensions. Apply GNOME shell theme. 
+- Configure display scaling and mouse sensitivity to your liking.
+- Set up the GRUB boot order if needed.
+
+## Using this script for yourself
+
+This script significantly simplifies the initial setup of an Ubuntu workstation, making it ideal for developers and general users looking to streamline their environment configuration but several steps are suited for my personal preference and comfort. For any issues or further customization, feel free to modify the script as needed.
+
+## Wallpapers
+
+![1](wallpapers/1.jpg)
+
+![2](wallpapers/2.jpg)
+
+![3](wallpapers/3.jpg)
+
+![4](wallpapers/4.jpg)
+
+![5](wallpapers/5.jpg)
+
+![6](wallpapers/6.jpg)
+
+![7](wallpapers/7.jpg)
+
+## TODO:
+[ ] Avoid offline installation of applications 
+
+[ ] Automate the installation of GNOME extensions if possible
---
+++ setup.sh
@@ -0,0 +1,171 @@
+#!/usr/bin/env bash
+
+  line_break() {
+printf "\n"
+}
+
+ask_user_to_execute_command() {
+    if [ "$#" = 3 ]; then
+        QUESTION=$1
+        COMMAND=$2
+        NO_MESSAGE=$3
+        while true; do
+            read -r -p "$QUESTION [Y/n] " yn
+            case $yn in
+                [Yy]*)
+                    $COMMAND
+                    break
+                    ;;
+                "")
+                    $COMMAND
+                    break
+                    ;; # Default choice: execute command
+                [Nn]*)
+                    echo "$NO_MESSAGE"
+                    break
+                    ;;
+                *) echo "Please answer yes or no." ;;
+            esac
+        done
+    else
+        echo "Call me as 'question' 'command to execute' 'Message if no is chosen'"
+    fi
+}
+
+say() { 
+line_break
+line_break 
+printf "$1"
+line_break
+}
+
+
+install_system_programs() {
+# Install gnome tweaks,grub-customizer and flatpak
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo apt update
+sudo apt upgrade
+sudo apt install -y gnome-tweaks gnome-shell-extension-manager grub-customizer neofetch flatpak curl
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash # Install nvm
+say "Rebooting..."
+sleep 3
+reboot
+}
+
+general_theming(){
+printf " Choose your wallpaper.
+1. Ash and Pikachu
+2. Son Goku
+3. Gon and Kilua
+4. Your Name Sceneryss
+5. Luffy Gear V
+6. Miyamoto Musashi
+7. Vinland Saga \n"
+read -r -p  "Enter the number of the wallpaper you want to use: " id
+gsettings set org.gnome.desktop.background picture-uri ~/Downloads/workstation-config/wallpapers/$id.jpg
+
+# Installing Fonts
+mkdir -p ~/.fonts
+cp ./fonts/SF-Pro-Display-Regular.otf ~/.fonts
+cp ./fonts/SF-Pro-Display-Bold.otf ~/.fonts
+
+# Installing gtk and shell themes
+mkdir -p ~/.themes
+cp -r ./themes/WhiteSur-Dark ~/.themes/
+cp -r ./themes/WhiteSur-Light ~/.themes/
+gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-Dark" # Apply WhiteSur-Dark gtk theme
+
+# Installing Icon Themes
+mkdir -p ~/.icons
+cp -r ./themes/WhiteSur-Icon ~/.icons/
+gsettings set org.gnome.desktop.interface icon-theme "WhiteSur-Icon"
+
+# Installing Cursor Theme
+cp -r ./themes/WhiteSur-cursors ~/.icons/
+gsettings set org.gnome.desktop.interface cursor-theme "WhiteSur-cursors"
+
+}
+
+install_applications() {
+say "Installing browsers and media players..."
+flatpak install flathub io.github.zen_browser.zen
+flatpak install flathub io.github.flattool.Warehouse 
+flatpak install flathub io.bassi.Amberol # Audio Player
+flatpak install flathub app.fotema.Fotema # Photo Gallery
+flatpak install flathub com.github.rafostar.Clapper # Video Player
+sudo snap install --classic code # The OG VS Code
+flatpak install flathub xyz.armcord.ArmCord # Vencord - Discord Client
+line_break
+ask_user_to_execute_command "Do you want to install Warp Terminal?" "sudo apt install ./applications/warp-terminal.deb" "Skipping Warp."
+line_break
+ask_user_to_execute_command "Do you want to install OBS Studio?" "flatpak install flathub com.obsproject.Studio" "Skipping OBS Studio."
+line_break
+ask_user_to_execute_command "Do you want to install Blender?" "flatpak install flathub org.blender.Blender" "Skipping Blender."
+line_break
+}
+
+install_runtime_env(){
+say "Installing development tools and runtimes.."
+line_break
+ask_user_to_execute_command "Do you want to install build-essential?" "sudo apt update && sudo apt install build-essential" "Skipping build-essential."
+line_break
+ask_user_to_execute_command "Do you want to install MongoDB Compass?" "flatpak install flathub com.mongodb.Compass" "Skipping MongoDB Compass."
+line_break
+
+# Nodejs installation through nvm
+nvm install 22
+node -v
+npm -v
+line_break
+ask_user_to_execute_command "Do you want to install JDK 21?" "sudo apt install openjdk-21-jdk" "Skipping Java Development Kit."
+line_break
+}
+
+git_setup(){
+sudo apt update && sudo apt install git
+printf "Installed Git Version: " git --version
+say "Configuring Git..."
+read -r -p  "Enter your username for git: " git_name
+read -r -p  "Enter your email for git: " git_email
+git config --global user.name "$git_name"
+git config --global user.email "$git_email"
+git config --global core.excludesFile ${pwd}/config/global_gitignore
+}
+
+
+manual_setup() {
+printf "
+1. Install following GNOME extensions: User Themes, Blur my Shell, Dash to Dock, GS Connect.\n
+2. Apply GNOME shell theme and change the font style. \n
+3. Configure display scaling and mouse sensitivity to your liking. \n
+4. Set up the GRUB boot order if needed. \n"
+}
+
+# Execution Order
+printf "This script sets up your OS with a reasonable config and programs. 
+For more details, see README.md.\n
+  Essential Softwares & Theming
+  Install application programs
+  Install compilers and runtime environments
+  Git configuration
+  \n Note: Before running the script, make sure the script is executed in the following directory: '~/Downloads/workstation-config' \n \n "
+  
+  ask_user_to_execute_command "Do you want to setup a fresh install with flatpak and gnome tweaks?" install_system_programs "Skipping flatpak and gnome tweaks. \n"
+  
+  general_theming
+  install_applications
+  install_runtime_env
+  ask_user_to_execute_command "\n Do you want to setup Git?" git_setup "Skipping Git setup. \n"
+  manual_setup
+  
+  
+  
+  
+  
+  
+ 
+ 
+
+
+
